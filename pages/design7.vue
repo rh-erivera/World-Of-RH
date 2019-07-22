993b08f15432b7bff9fbf31236be5499bb56a5c7
Eugene Rivera <erivera@rh-er-21434.restorationhardware.com>
2019-07-22 14:02:56 -0700

    design 7 v1

---
+++ pages/design7.vue
@@ -0,0 +1,257 @@
+<template>
+    <div>
+        <div class="nav-background-images" id="bg"></div>
+        <div class="container">
+            
+            <div class="nav">
+                <p class="shop" @mouseover="showShop" @mouseleave="hideShop">SHOP</p>
+                <img id="logo" src="/theWorldOfRH-Logo.svg" alt="">
+                <p class="explore" @mouseover="showExplore" @mouseleave="hideExplore">EXPLORE</p>
+            </div>
+            <div id="shop-dropdown" class="nav-links shop-dropdown" @mouseover="showShop" @mouseleave="hideShop">
+                <a href="">RH INTERIORS</a><br>
+                <a href="">RH MODERN</a><br>
+                <a href="">RH BABY & CHILD</a><br>
+                <a href="">RH TEEN</a><br>
+                <a href="">RH BEACH HOUSE</a><br>
+                <a href="">RH SKI HOUSE</a><br>
+                <a href="">SOURCEBOOKS</a>
+            </div>
+            <div id="explore-dropdown" class="nav-links explore-dropdown" @mouseover="showExplore" @mouseleave="hideExplore">
+                <a href="">GALLERIES</a><br>
+                <a href="">RESTAURANTS</a><br>
+                <a href="">RESTAURANTS</a><br>
+                <a href="">RESIDENCES</a><br>
+                <a href="">GUEST HOUSES</a><br>
+                <a href="">PLANES</a><br>
+                <a href="">YATCHS</a><br>
+                <a href="">DESIGN SERVICES</a>
+            </div>
+            <!-- <div class="nav list-view-items" id="nav">
+                <a href="" id="item-1" @mouseover="stopSlideImage(1)" @mouseleave="startSlideImage(1)" style="color: black; border-bottom: transparent;">collections</a>
+                <a href="" id="item-2" @mouseover="stopSlideImage(2)" @mouseleave="startSlideImage(2)">sourcebooks</a>
+                <a href="" id="item-3" @mouseover="stopSlideImage(3)" @mouseleave="startSlideImage(3)">design services</a>
+                <a href="" id="item-4" @mouseover="stopSlideImage(4)" @mouseleave="startSlideImage(4)">galleries</a>
+                <a href="" id="item-5" @mouseover="stopSlideImage(5)" @mouseleave="startSlideImage(5)">restaurants</a>
+                <a href="" id="item-6" @mouseover="stopSlideImage(6)" @mouseleave="startSlideImage(6)">residences</a>
+                <a href="" id="item-7" @mouseover="stopSlideImage(7)" @mouseleave="startSlideImage(7)">guesthouses</a>
+                <a href="" id="item-8" @mouseover="stopSlideImage(8)" @mouseleave="startSlideImage(8)">planes</a><br>
+                <a href="" id="item-9" @mouseover="stopSlideImage(9)" @mouseleave="startSlideImage(9)">yachts</a>
+            </div> -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            i: 2,
+            hideItemElement: {},
+            showItemElement: {},
+            showBg: {},
+            startSlideShowInterval: {},
+            interval: null
+        }
+    },
+    mounted() {
+        this.hideItemElement = document.getElementById('item-1')
+        this.showItemElement = document.getElementById('item-1')
+        this.showBg = document.getElementById('bg')
+        // setTimeout(()=> {
+        //     document.getElementById('logo').style.opacity = 1
+        // }, 10000)
+        // setTimeout(()=> {
+        //     document.getElementById('nav').style.opacity = 1
+        // }, 11000)
+        this.startSlideShowInterval = setInterval(this.slideshow, 4000)
+    },
+    methods: {
+        // startSlideShow() {
+        //     clearInterval(this.startSlideShowInterval)
+        //     // this.showBg.style.opacity = 1;
+        //     // this.showItemElement.style.borderBottom = '1px solid #ccc'
+        //     this.interval = setInterval(this.slideshow, 4000)
+        // },
+        slideshow() {
+            // if (this.i == 1) {
+            //     this.hideItemElement = document.getElementById('item-'+(this.i+8))
+            //     this.showItemElement = document.getElementById('item-'+(this.i))
+            // } else {
+            //     this.hideItemElement = document.getElementById('item-'+(this.i-1))
+            //     this.showItemElement = document.getElementById('item-'+(this.i))
+            // }
+            this.showBg.style.backgroundImage = 'url(\'/design1/01_bg_'+this.i+'.jpg\')';
+            // this.hideItemElement.style.borderBottom = 'transparent'
+            // this.showItemElement.style.borderBottom = '1px solid #ccc'
+            if (this.i == 9) {
+                this.i = 1
+            } else {
+                this.i++
+            }
+        },
+        showShop() {
+            document.getElementById('shop-dropdown').style.opacity = 1;
+        },
+        hideShop() {
+            document.getElementById('shop-dropdown').style.opacity = 0;
+        },
+        showExplore() {
+            document.getElementById('explore-dropdown').style.opacity = 1;
+        },
+        hideExplore() {
+            document.getElementById('explore-dropdown').style.opacity = 0;
+        }
+        // stopSlideImage(i) {
+        //     clearInterval(this.interval)
+        //     this.showItemElement.style.borderBottom = 'transparent'
+        //     document.getElementById('item-'+i).style.borderBottom = '1px solid #ccc';
+        //     this.showBg.style.backgroundImage = 'url(\'/design1/01_bg_'+i+'.jpg\')';
+        // },
+        // startSlideImage(i) {
+        //     document.getElementById('item-'+i).style.borderBottom = 'transparent';
+        //     this.showItemElement.style.borderBottom = '1px solid #ccc'
+        //     if (this.i == 1) {
+        //         this.showBg.style.backgroundImage = 'url(\'/design1/01_bg_'+(this.i)+'.jpg\')';
+        //     } else {
+        //         this.showBg.style.backgroundImage = 'url(\'/design1/01_bg_'+(this.i-1)+'.jpg\')';
+        //     }
+        //     this.interval = setInterval(this.slideshow, 4000)
+        // }
+  }
+}
+</script>
+
+<style>
+.container {
+    height: 665px;
+    margin: 0 auto;
+    max-width: 1490px;
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+}
+
+.nav {
+    font-family: 'CaslonRH-Superfine', sans-serif;
+    font-size: 18pt;
+    line-height: 24pt;
+    letter-spacing: .04em;
+    color: #333;
+    animation: fadeIn 2s ease-in;
+}
+
+#logo {
+    width: 85px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 41.5%;
+    margin: auto;
+}
+
+.shop {
+    position: absolute;
+    left: 20%;
+    top: 46%;
+}
+
+.explore {
+    position: absolute;
+    right: 20%;
+    top: 46%;
+}
+
+.nav-links {
+    font-family: 'BaronSans-Thin', sans-serif;
+    font-size: 9pt;
+    line-height: 18pt;
+    letter-spacing: .07em;
+    color: #333;
+}
+
+.shop-dropdown {
+    position: absolute;
+    opacity: 0;
+    transition: opacity 750ms ease-in;
+    left: 20%;
+    top: 52.5%;
+}
+
+.explore-dropdown {
+    position: absolute;
+    opacity: 0;
+    transition: opacity 750ms ease-in;
+    right: 20%;
+    top: 52.5%;
+}
+
+.nav p {
+    /* position:relative; */
+    transition: 0.5s;
+    border-bottom: transparent;
+}
+
+.nav p::before{
+    width: 0%;
+    height:100%;
+    z-index: 3;
+    content:'';
+    position: absolute;
+    bottom: -1px;
+    box-sizing: border-box;
+    transition: .5s;
+ }
+.nav p:hover::before {
+    width: 100% !important;
+    transition: .5s;
+ }
+.nav p::before {
+    border-bottom: 1px solid #333;
+}
+
+.nav-background-images {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100%;
+    background-image: url('/design1/01_bg_1.jpg');
+    background-position: center;
+    background-size: cover;
+    animation: fadeIn 2s ease-in;
+    transition: background-image 750ms ease-in, opacity 750ms ease-in;
+    -webkit-transition: background-image 750ms ease-in, opacity 750ms ease-in;
+}
+.list-view-items a {
+  transition: color 750ms ease-in, border-bottom 750ms ease-in;
+  -webkit-transition: color 750ms ease-in, border-bottom 750ms ease-in;
+}
+
+@media (min-width: 1025px) {
+    .RH-Caslon {
+        font-size: 55pt;
+        line-height: 45pt;    
+    }
+    .Baron-Sans {
+        font-size: 20pt;
+        line-height: 45pt;
+    }
+    .nav {
+        width: 275px;
+    }
+    .nav a {
+        font-size: 20pt;
+        line-height: 48pt;
+    }
+}
+
+@keyframes fadeIn {
+    0% {opacity: 0;}
+    100% {opacity: 1;}
+}
+@-webkit-keyframes fadeIn {
+    0% {opacity: 0;}
+    100% {opacity: 1;}
+}
+</style>
